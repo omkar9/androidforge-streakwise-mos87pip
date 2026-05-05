171f1141c2fb664bd103fca35cf72fb7f88cdbe0
omkar9 <omkar946@gmail.com>
2026-05-05 11:39:26 +0530

    feat: add MarkHabitCompletedUseCase.kt

---
+++ domain/src/main/java/com/androidforge/streakwise/domain/usecase/completions/MarkHabitCompletedUseCase.kt
@@ -0,0 +1,15 @@
+package com.androidforge.streakwise.domain.usecase.completions
+
+import com.androidforge.streakwise.domain.model.HabitCompletion
+import com.androidforge.streakwise.domain.repository.HabitCompletionRepository
+import java.time.LocalDate
+import javax.inject.Inject
+
+class MarkHabitCompletedUseCase @Inject constructor(
+    private val habitCompletionRepository: HabitCompletionRepository
+) {
+    suspend operator fun invoke(habitId: Long, date: LocalDate) {
+        val completion = HabitCompletion(habitId = habitId, completionDate = date, isCompleted = true)
+        habitCompletionRepository.addOrUpdateHabitCompletion(completion)
+    }
+}
