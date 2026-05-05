b69cb4db01aae455fad76881dede01d878280a13
omkar9 <omkar946@gmail.com>
2026-05-05 11:40:59 +0530

    fix: update StreakWiseApp.kt

--- app/src/main/java/com/androidforge/streakwise/StreakWiseApp.kt
+++ app/src/main/java/com/androidforge/streakwise/StreakWiseApp.kt
@@ -1,21 +1,7 @@
 package com.androidforge.streakwise
 
 import android.app.Application
-import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
-class StreakWiseApp : Application() {
-    override fun onCreate() {
-        super.onCreate() 
-
-        // Initialize Timber for logging in debug builds
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-
-        // Initialize the Mobile Ads SDK
-        MobileAds.initialize(this) {}
-    }
-}
+class StreakWiseApp : Application()
