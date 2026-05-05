1623dc7af976762b81f3d24a76e62896f4ca205b
omkar9 <omkar946@gmail.com>
2026-05-05 11:38:09 +0530

    feat: add HabitsDashboardUiState.kt

---
+++ presentation/src/main/java/com/androidforge/streakwise/presentation/ui/habitsdashboard/HabitsDashboardUiState.kt
@@ -0,0 +1,6 @@
+package com.androidforge.streakwise.presentation.ui.habitsdashboard
+
+import com.androidforge.streakwise.domain.model.HabitWithCompletionStatus
+import com.androidforge.streakwise.presentation.ui.common.UiState
+
+typealias HabitsDashboardUiState = UiState<List<HabitWithCompletionStatus>>
